c3287f9b8688674de7d3039e0d857edf4ada3636
Rally Jinx <rsjinx@gmail.com>
2017-08-21 22:25:12 -0500

    tiny little changes to see if heroku works

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Log requests to the console.
